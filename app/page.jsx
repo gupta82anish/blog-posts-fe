f0db5431a28f5ea09ece874695e03a218bbd1295
Anish Gupta <gupta.anish82@yahoo.com>
2023-09-10 06:33:10 +0200

    added message for no posts found

--- app/page.jsx
+++ app/page.jsx
@@ -27,6 +27,20 @@ export default function PostsList(){
       fetchPosts();
     }, [page]);
 
+    // conditionally render the h3 tag saying no posts found if total == 0
+    if(posts.length == 0){
+      return (
+        <div className="container mx-auto p-4">
+          <h3 className="text-center text-2xl md:text-3xl font-bold mb-4">No Posts Found</h3>
+          <div className="flex justify-center mt-6">
+              <Link className="top-0 md:top-50 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition-colors duration-300"
+                href="/posts/addBlogPost">Write Something Good</Link>
+            </div>
+          </div>
+      )
+    }
+    else {
+
     return (
         <div className="container mx-auto p-4">
           <h1 className="text-center text-2xl md:text-3xl font-bold mb-4">Posts are here</h1>
@@ -45,6 +59,7 @@ export default function PostsList(){
           </div>
         </div>
     );
+              }
 }
 
 function Post({ post }) {
