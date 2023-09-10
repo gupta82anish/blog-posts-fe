22e47b17f0ebf612fdf6902fcdd484967f5e1f55
Anish Gupta <gupta.anish82@yahoo.com>
2023-09-10 06:09:25 +0200

    Pagination added and working

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,32 @@
+'use client';
 import Link from "next/link";
-async function getPosts() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`, {cache: 'no-store'});
+import { useState, useEffect } from "react";
+
+async function getPosts(page, limit= 10) {
+  console.log('page', page)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts?$limit=10&$skip=${(page - 1)*10}`, {cache: 'no-store'});
     const data = await res.json();
-    return data?.data;
+    return data;
   }
   
-export default async function PostsList(){
-    const posts = await getPosts();
+export default function PostsList(){
+    // const posts = await getPosts();
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [disableNext, setDisableNext] = useState(false);
+    useEffect(() => {
+      const fetchPosts = async () => {
+        const data = await getPosts(page);
+        const total = data?.total;
+        console.log('total', total)
+        const limit = data?.limit
+        total > limit ? setDisableNext(false) : setDisableNext(true);
+        data?.data.length < 10 ? setDisableNext(true) : setDisableNext(false);
+        setPosts(data?.data);
+      }
+      fetchPosts();
+    }, [page]);
+
     return (
         <div className="container mx-auto p-4">
           <h1 className="text-center text-2xl md:text-3xl font-bold mb-4">Posts are here</h1>
@@ -18,6 +38,10 @@ export default async function PostsList(){
           <div className="flex justify-center mt-6">
             <Link className="top-0 md:top-50 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition-colors duration-300"
               href="/posts/addBlogPost">Write Something Good</Link>
+          </div>
+          <div className="flex justify-between mt-4">
+            <button disabled={page===1} onClick={() => setPage(prev => Math.max(prev - 1, 1))}> &lt; Previous</button>
+            <button disabled={disableNext} onClick={() => setPage(prev => prev + 1)}>Next &gt;</button>
           </div>
         </div>
     );
